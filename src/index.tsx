4a39f70072513567b941498acff9c953ab8cbc44
Taehong Kim <lkoiescg2031@naver.com>
2021-03-01 00:26:31 +0900

    [Add] redux 스토어 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import configureStore from "./redux/configureStore";
 
 import "fontsource-roboto";
 
-import './index.css';
+import "./index.css";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider store={configureStore()}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/configureStore.ts
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import reducers from "./reducers";
+
+// import thunk from "redux-thunk";
+// import { middleware as reduxPackMiddleware } from "redux-pack";
+const configureStore = (initStates?: {}) =>
+	createStore(
+		combineReducers(reducers),
+		initStates,
+		composeWithDevTools(
+			applyMiddleware()
+			// thunk,
+			// reduxPackMiddleware,
+		)
+	);
+
+export default configureStore;
